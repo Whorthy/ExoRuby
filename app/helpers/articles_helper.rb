36c737b27888b01f6b340e31d2a5df0b9bebb500
Tom Moraiz <tom.moraiz@hetic.net>
2017-01-30 15:59:03 +0100

    [MOD] couter_color, toBeFixed

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,7 @@
 module ArticlesHelper
+
+
+
+
 end
+
--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
 	belongs_to :chapter 
 
 	scope :published, -> { where.not(published_at: nil)}
+
+	def to_param
+		"#{id}-#{name.parameterize}"
+	end
 end
