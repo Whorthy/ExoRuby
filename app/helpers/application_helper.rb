be47269c9d4f49637f46c62e97ac221bbd512a0d
Tom Moraiz <tom.moraiz@hetic.net>
2017-01-30 10:26:36 +0100

    [MOD] implemented date.nil failsafe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,19 @@
 module ApplicationHelper
 	def sexy_date(date)
-		return date.strftime("%d/%m/%Y")
+		return date.try(:strftime, "%d/%m/%Y")
+	end
+
+# 	if date.nil? 
+# 		return "Date inconnue"
+# 	else 
+# 		return date.strftime("%d/%m/%Y")
+# 	end
+
+# 	return date.nil ? "Date inconnue" : date.strftime("%d/%m/%Y")
+
+# 	return date.strftime("%d/%m/%Y") rescue "Date inconnue"
+
+	def is_published(date)
+		return date.time_ago_in_words rescue "Fuck this shit m8"
 	end
 end
